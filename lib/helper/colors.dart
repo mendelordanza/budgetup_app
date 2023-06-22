ae9af8ad89e6908e8e3bd40e935c44439d32bb95
ralphordanza <mendelordanza@gmail.com>
2023-06-22 13:39:39 +0800

    update primary color

--- lib/helper/colors.dart
+++ lib/helper/colors.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 const darkBackgroundColor = Color(0xFF1D1D1D);
 const lightBackgroundColor = Color(0xFFF3F3F3);
 
-const primaryColor = Color(0xFFDE5400);
+const primaryColor = Color(0xFFE46B22);
 const secondaryColor = Color(0xFFFFA559);
 const dark = Color(0xFF333333);
 const red = Color(0xFFDD432E);
--- lib/presentation/custom/date_filter_button.dart
+++ lib/presentation/custom/date_filter_button.dart
@@ -1,7 +1,6 @@
+import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
-import '../../helper/colors.dart';
 
 class DateFilterButton extends StatelessWidget {
   final String text;
@@ -12,24 +11,38 @@ class DateFilterButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 24.0),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: SvgPicture.asset(
-              "assets/icons/ic_calendar.svg",
-              color: Theme.of(context).primaryColor,
-            ),
+      child: Material(
+        shape: SmoothRectangleBorder(
+          borderRadius: SmoothBorderRadius(
+            cornerRadius: 8,
+            cornerSmoothing: 1.0,
           ),
-          Text(
-            text,
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w500,
-              color: Theme.of(context).primaryColor,
-            ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 10.0,
           ),
-        ],
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: SvgPicture.asset(
+                  "assets/icons/ic_calendar.svg",
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
