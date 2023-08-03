4c6da6bdb2e016c557eed253c2c59a3749f88683
ralphordanza <mendelordanza@gmail.com>
2023-08-03 11:15:22 +0800

    feat: ask review after opening 5 times

--- lib/main.dart
+++ lib/main.dart
@@ -73,11 +73,12 @@ class _MyAppState extends State<MyApp> {
 
   shouldShowReview() async {
     var counter = sharedPrefs.getShouldShowReview();
-    if (counter == 5) {
+    if (counter >= 5) {
       final inAppReview = InAppReview.instance;
       if (await inAppReview.isAvailable()) {
         inAppReview.requestReview();
       }
+      sharedPrefs.setShouldShowReview(0);
     } else {
       sharedPrefs.setShouldShowReview(counter++);
     }
